2ad933b6f7791da0e6a4acd7a2b6655541c7b1e9
davidhung <davidhung@ITL0595>
2025-01-20 16:15:24 +0800

    feat: add bar chart for data visualization

--- NutritionApp/Components/Charting/DataSet.cs
+++ NutritionApp/Components/Charting/DataSet.cs
@@ -4,6 +4,6 @@ public class Dataset
     public string? Label { get; set; }
     public string? BackgroundColor { get; set; }
     public string? BorderColor { get; set; }
-    public int[]? Data { get; set; }
+    public double[]? Data { get; set; }
     public int BorderWidth { get; set; }
 }
